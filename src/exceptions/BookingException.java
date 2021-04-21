f47cc459e4d218880829fa394bcaa13988092c2a
İbrahim Bilal Gürhan <i.b.gurhan@hotmail.com>
2021-04-21 09:06:14 +0100

    second commit

---
+++ src/exceptions/BookingException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class BookingException extends Exception {
+
+    public BookingException(String message) {
+        super(message);
+    }
+}
---
+++ src/exceptions/TreatmentBookedException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class TreatmentBookedException extends Exception {
+
+    public TreatmentBookedException(String message) {
+        super(message);
+    }
+}
