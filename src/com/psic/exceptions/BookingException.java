e684a67ac4c882e3c7e427b374b64ec1b5fd9731
İbrahim Bilal Gürhan <i.b.gurhan@hotmail.com>
2021-04-25 10:10:11 +0100

    fixed treatment, booking and main class

--- src/com/psic/exceptions/BookingException.java
+++ src/com/psic/exceptions/BookingException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package com.psic.exceptions;
 
 public class BookingException extends Exception {
 
--- src/com/psic/exceptions/TreatmentBookedException.java
+++ src/com/psic/exceptions/TreatmentBookedException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package com.psic.exceptions;
 
 public class TreatmentBookedException extends Exception {
 
